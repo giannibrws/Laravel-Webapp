4a0e95e36062e9247d5e7a02bfd778b7de14c56a
Gianni <gianni.brouwers@student.kdg.be>
2021-10-27 11:33:35 +0200

    Updated .readme + app routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -13,23 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// redirect to login route: 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 // Redirect to userController index function:
 
+// Only authenticated users may access this route...
+Route::group(['middleware' => 'auth'], function () {
 
+    Route::get('/', 'UserController@index')->name('home');
+    Route::get('/home', 'UserController@index')->name('home');
 
-Route::get('/home', 'UserController@index')->name('home');
+    // Assign crud routes to userController:
+    Route::resource('User', 'UserController');
+    Route::resource('User/?', 'HomeController');
+    Route::resource('Home', 'HomeController');
+    
+    Route::get('/create', 'HomeController@create')->name('create');
+    Route::get('/edit', 'HomeController@edit')->name('edit');
 
-
-// Assign crud routes to userController:
-Route::resource('User', 'UserController');
-Route::resource('User/?', 'HomeController');
-Route::resource('Home', 'HomeController');
-
-Route::get('/create', 'HomeController@create')->name('create');
-Route::get('/edit', 'HomeController@edit')->name('edit');
-
+});
